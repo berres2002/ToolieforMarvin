fd1ee0123c690cbf918ddc12ead6d9b298106317
Aidan Berres <48331027+berres2002@users.noreply.github.com>
2020-01-30 01:12:54 -0800

    add with updated center and image viewer

--- unwise.py
+++ unwise.py
@@ -1,5 +1,9 @@
 from astropy.table import Table
 from astropy.io import fits
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
+from astropy.utils.data import get_pkg_data_filename
 #from astropy.coordinates import SkyCoord
 #import requests
 #from PIL import Image
@@ -18,10 +22,6 @@ def getFits(plate):
 
     url1='https://irsa.ipac.caltech.edu/ibe/search/wise/allwise/p3am_cdd?POS={},{}'
 
-    #e=requests.get(url1.format(ra,dec))
-    #file=open('f.txt','w+')
-    #file.write(e.text)
-    #file.close()
     try:
         re=Table.read(url1.format(ra,dec),format='ipac')
     except:
@@ -31,14 +31,12 @@ def getFits(plate):
     params['coaddgrp'] = params['coadd_id'][:2]
     params['coadd_ra'] = params['coadd_id'][:4]
     path = str.format(
-        '{coaddgrp:s}/{coadd_ra:s}/{coadd_id:s}/{coadd_id:s}-w{band:1d}-int-3.fits',
+        '{coaddgrp:s}/{coadd_ra:s}/{coadd_id:s}/{coadd_id:s}-w{band:1d}-int-3.fits?',
         **params)
+    path=path+str.format('center={},{}&size=200pix',ra,dec)
+
     url = 'https://irsa.ipac.caltech.edu/ibe/data/wise/allwise/p3am_cdd/' + path
 
-    #y=requests.get(url)
-    #with open('7443-12701.fits', 'w+') as fd:
-    #    for chunk in y.iter_content(chunk_size=128):
-    #        fd.write(str(chunk))
     try:
         f2=fits.open(url)
     except:
@@ -47,12 +45,20 @@ def getFits(plate):
         f2.writeto(filePath+plate+'.fits')
     except:
         raise ValueError('Something went wrong saving the file to the folder "'+filePath+'" for plate-ifu: '+plate)
-    #print(f[0].header)
-    #file=open('f.txt','w+')
-    #file.write('')
-    #file.close()
+
     try:
         fits.open(filePath+plate+'.fits')
     except:
         raise ValueError('Something went wrong trying to open the file for plate-ifu: '+plate)
     print('Saved successfully as: '+plate+'.fits')
+
+def viewImage(fN):
+    try:
+        image_file = get_pkg_data_filename(filePath+fN)
+    except:
+        raise ValueError('Something went wrong accessing the file,\n '
+                         'make sure the .fits file is downloaded, you can use findFits() to get the image')
+    image_data = fits.getdata(image_file, ext=0)
+    plt.imshow(image_data, cmap='gray')
+    plt.title(fN)
+    plt.colorbar()
